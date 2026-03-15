074174dd7bf751d0281862d25597d56c51194065
Vanshika <vanshikaraje010@gmail.com>
2026-03-15 12:26:34 +0530

    Time: 3 ms (46.98%) | Memory: 9.8 MB (23.56%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<int>freq(26,0);
+        if(s.size()!=t.size())
+        return false;
+        for(int i = 0;i<s.size();i++){
+            freq[s[i]- 'a']++;
+            freq[t[i]- 'a']--;
+        }
+        for(int i = 0;i<26;i++){
+            if(freq[i]!=0)return false;
+        }
+        return true;
+    }
+};
