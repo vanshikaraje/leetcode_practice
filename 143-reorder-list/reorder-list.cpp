907bf9f6c0e616b325e05b11fcc47ca3af939ded
Vanshika <vanshikaraje010@gmail.com>
2026-04-23 05:51:21 +0530

    Time: 0 ms (100.00%) | Memory: 22.9 MB (52.77%) - LeetSync

---
+++ 143-reorder-list/reorder-list.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        
+        if(head == NULL || head->next == NULL) return;
+
+        // 1. Find middle
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast->next != NULL && fast->next->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // 2. Reverse second half
+        ListNode* second = slow->next;
+        slow->next = NULL;  // break list
+
+        ListNode* prev = NULL;
+        while(second != NULL) {
+            ListNode* next = second->next;
+            second->next = prev;
+            prev = second;
+            second = next;
+        }
+
+        // 3. Merge alternately
+        ListNode* first = head;
+        second = prev;
+
+        while(second != NULL) {
+            ListNode* temp1 = first->next;
+            ListNode* temp2 = second->next;
+
+            first->next = second;
+            second->next = temp1;
+
+            first = temp1;
+            second = temp2;
+        }
+    }
+};
