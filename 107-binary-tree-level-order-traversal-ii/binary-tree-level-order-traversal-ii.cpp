d70d4b070cd69015992c6a4c33a04063dba5a3ac
Vanshika <vanshikaraje010@gmail.com>
2026-07-04 15:43:05 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (94.21%) - LeetSync

--- 107-binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.cpp
+++ 107-binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.cpp
@@ -1,40 +1,39 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void bfs(TreeNode* root,vector<vector<int>>&ans){
-        if(root==NULL)
-        return;
-        queue<TreeNode*>q;
-        q.push(root);
-        while(!q.empty()){
-            int size = q.size();
-            vector<int>level;
-            for(int i = 0;i<size;i++){
-                TreeNode* temp = q.front();
-                q.pop();
-                level.push_back(temp->val);
-                if(temp->left)
-                q.push(temp->left);
-                if(temp->right)
-                q.push(temp->right);
-            }
-           ans.push_back(level);
-        }
-    }
     vector<vector<int>> levelOrderBottom(TreeNode* root) {
-        vector<vector<int>>ans;
-        bfs(root,ans);
-        reverse(ans.begin(),ans.end());
+
+        vector<vector<int>> ans;
+
+        if(root == NULL)
+            return ans;
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while(!q.empty()) {
+
+            int size = q.size();
+            vector<int> level;
+
+            for(int i = 0; i < size; i++) {
+
+                TreeNode* node = q.front();
+                q.pop();
+
+                level.push_back(node->val);
+
+                if(node->left)
+                    q.push(node->left);
+
+                if(node->right)
+                    q.push(node->right);
+            }
+
+            ans.push_back(level);
+        }
+
+        reverse(ans.begin(), ans.end());
+
         return ans;
     }
-}; 
+};
