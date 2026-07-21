5c19da43c69ef995af9950c57fcb51c1eef6560f
Vanshika <vanshikaraje010@gmail.com>
2026-07-21 17:48:14 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (42.46%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,60 +1,21 @@
-// class Solution {
-// public:
-//     void nextPermutation(vector<int>& nums) {
-//        int n = nums.size();
-//     int index = -1;
-
-//     // Step 1: find pivot (first number from right which is smaller)
-//     for (int i = n - 2; i >= 0; i--) {
-//         if (nums[i] < nums[i + 1]) {
-//             index = i;
-//             break;
-//         }
-//     }
-
-//     // If no pivot found, reverse whole array
-//     if (index == -1) {
-//         reverse(nums.begin(), nums.end());
-//         return;
-//     }
-
-//     // Step 2: find the smallest number greater than nums[index]
-//     for (int i = n - 1; i > index; i--) {
-//         if (nums[i] > nums[index]) {
-//             swap(nums[i], nums[index]);
-//             break;
-//         }
-//     }
-
-//     // Step 3: reverse the right part
-//     reverse(nums.begin() + index + 1, nums.end());
-// }
-// };
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-    int index  =-1;
-    int n = nums.size();
 
-    //  for finding the pivot element
-    for(int i = n-2;i>=0;i--){
-        if(nums[i]<nums[i+1]){
-            index = i;
-            break;
+        int n = nums.size();
+        int i = n - 2;
+        while(i >= 0 && nums[i] >= nums[i + 1]){
+            i--;
         }
-    }
-    // if pivot doesnot found
-    if(index == -1){
-        reverse(nums.begin(),nums.end());
-        return ;
-    }
-    // find smallest elemet 
-    for(int i = n-1;i>index;i--){
-        if(nums[i]>nums[index]){
-            swap(nums[i],nums[index]);
-            break;
+        if(i >= 0){
+            int j = n - 1;
+
+            while(nums[j] <= nums[i]){
+                j--;
+            }
+
+            swap(nums[i], nums[j]);
         }
+        reverse(nums.begin() + i + 1, nums.end());
     }
-    reverse(nums.begin()+index+1,nums.end());
-}
 };
