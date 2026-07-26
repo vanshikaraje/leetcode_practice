c8521efe2d20624464b0ead34514b7f05cf903cc
Vanshika <vanshikaraje010@gmail.com>
2026-07-26 17:54:42 +0530

    Time: 19 ms (80.82%) | Memory: 17.4 MB (84.91%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,34 +1,78 @@
 class Solution {
 public:
-    vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        vector<vector<int>>ans;
-        sort(nums.begin(),nums.end());
-        for(int i = 0;i<nums.size();i++){
-            if(i>0&&nums[i]==nums[i-1])continue;
-            for(int j = i+1;j<nums.size();j++){
-                if(j>i+1&&nums[j]==nums[j-1])continue;
 
-                int left = j+1;
-                int right = nums.size()-1;
+vector<vector<int>> fourSum(vector<int>& nums, int target) {
 
-                while(left<right){
-                    long long sum = (long long)nums[i]+nums[j]+nums[left]+nums[right];
-                    if(sum==target){
-                        ans.push_back({nums[i],nums[j],nums[left],nums[right]});
-                        while(left<right&&nums[left]==nums[left+1])left++;
-                        while(left<right&&nums[right]==nums[right-1])right--;
+    vector<vector<int>> ans;
+
+    int n = nums.size();
+
+    // Step 1: Sort
+    sort(nums.begin(), nums.end());
+
+    // Step 2: Fix first number
+    for (int i = 0; i < n - 3; i++) {
+
+        // Skip duplicate i
+        if (i > 0 && nums[i] == nums[i - 1]) {
+            continue;
+        }
+
+        // Step 3: Fix second number
+        for (int j = i + 1; j < n - 2; j++) {
+
+            // Skip duplicate j
+            if (j > i + 1 && nums[j] == nums[j - 1]) {
+                continue;
+            }
+
+            // Step 4: Two pointers
+            int left = j + 1;
+            int right = n - 1;
+
+            while (left < right) {
+
+                long long sum = (long long)nums[i] 
+                              + nums[j] 
+                              + nums[left] 
+                              + nums[right];
+
+                if (sum == target) {
+
+                    ans.push_back({
+                        nums[i],
+                        nums[j],
+                        nums[left],
+                        nums[right]
+                    });
+
+                    left++;
+                    right--;
+
+                    // Skip duplicate left
+                    while (left < right && 
+                           nums[left] == nums[left - 1]) {
                         left++;
-                        right--;
                     }
-                    else if(sum<target){
-                        left++;
-                    }
-                    else{
+
+                    // Skip duplicate right
+                    while (left < right && 
+                           nums[right] == nums[right + 1]) {
                         right--;
                     }
                 }
+
+                else if (sum < target) {
+                    left++;
+                }
+
+                else {
+                    right--;
+                }
             }
         }
-        return ans;
     }
+
+    return ans;
+}
 };
